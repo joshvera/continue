973b8b7eaa201e54dd88cfedff5360a687166776
Josh Vera <josh@joshvera.com>
2024-07-21 15:56:38 -0400

    Always refreshSubmenuItems in refreshCodebaseIndex

--- core/core.ts
+++ core/core.ts
@@ -652,6 +652,8 @@ export class Core {
       this.messenger.request("indexProgress", update);
       this.indexingState = update;
     }
+
+    this.messenger.send("refreshSubmenuItems", undefined)
   }
 
   private async indexDocs(sites: SiteIndexingConfig[], reIndex: boolean): Promise<void> {
